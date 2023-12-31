bb8d0e95f4ba53b2849b7dd2d51335dcd32cac46
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2023-12-31 13:57:51 -0300

    cv2 first interactions

--- video.py
+++ video.py
@@ -1,1 +1,11 @@
+import cv2
+import image
 
+badAppleCapture = cv2.VideoCapture('Bad_Apple!!.mp4')
+
+if(not badAppleCapture.isOpened()):
+    print("That ain't working my dude")
+    exit()
+
+fps = badAppleCapture.get(5)
+print(fps)
